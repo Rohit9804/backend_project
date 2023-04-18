4880983f0015cd1fc0ecbcdc6101be726cc6a630
rohit9804 <rohit9804singh@gmail.com>
2023-04-19 01:30:34 +0530

    added api to update, delete and getall tasks.

--- controllers/task.js
+++ controllers/task.js
@@ -15,3 +15,49 @@ export const newTask = async(req,res,next) => {
         message : "task added successfully"
     });
 };
+
+
+export const getMyTask = async(req,res,next) => {
+
+    const userid = req.user.id;
+
+    const tasks = await Task.find ({ user : userid});
+    res.status(201).json({
+        success : true,
+        tasks,
+    });
+} 
+
+export const updateTask = async(req,res,next) => {
+
+    const task = await Task.findById(req.params.id);
+
+    if (!task) return res.status(404).json({
+        success : false,
+        message : "task not found",
+    })
+
+    task.isCompleted = !task.isCompleted
+    await task.save()
+
+    res.status(201).json({
+        success : true,
+        message : "task updated",
+    });
+}
+
+export const deleteTask = async(req,res,next) => {
+
+    const task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).json({
+        success : false,
+        message : "task not found"
+    })
+
+    await task.deleteOne();
+
+    res.status(201).json({
+        success : true,
+        message : "task deleted",
+    });
+}
