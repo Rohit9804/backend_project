15d46b2497cc39c9e4bed57ad61c54853ecb4e4c
rohit9804 <rohit9804singh@gmail.com>
2023-04-19 00:05:04 +0530

    added functionallity to add tasks

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express, { json } from "express";
 import  userRouter  from "./routes/user.js"
+import  taskRouter  from "./routes/task.js"
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 
@@ -13,7 +14,11 @@ app.use(userRouter);
 //middleware
 app.use(express.json());
 app.use(cookieParser());
+
+
+//using routes
 app.use("/api/v1/users", userRouter)
+app.use("/api/v1/task", taskRouter)
 
 app.get("/", (req,res) => {
     res.send("nice working");
